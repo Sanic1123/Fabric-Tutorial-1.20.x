b951a1f3bb5f45c6ac1d657cae38a493c2979436
Sanic <gassmannmr@yahoo.com>
2024-04-02 08:59:14 +0200

    Steel Ingot added

--- src/main/java/net/sanic/tutorialmod/TutorialMod.java
+++ src/main/java/net/sanic/tutorialmod/TutorialMod.java
@@ -2,17 +2,18 @@ package net.sanic.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sanic.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger("template-mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-
+		ModItems.registerModItems();
 
 	}
 }
